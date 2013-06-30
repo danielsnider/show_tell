787f02168199f47aa05f1b69df1032ab1dbc31be
Daniel Snider <danielsnider12@gmail.com>
2013-06-30 07:20:42 +0000

    files can be uploaded. they get saved to public/uploads. they get processed by routes/upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,7 @@
  * after POST file upload occures process the file
  */
 
-exports.user_file_upload = function(req, res) {
+exports.fileupload = function(req, res) {
 
 	if (req.files.file)  {
 		console.log("Recieved file: \n " + req.files.file);
--- server.js
+++ server.js
@@ -33,6 +33,11 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
+app.post('/file_upload', function(req, res, next) {
+    console.log(req.body);
+    console.log(req.files);
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
