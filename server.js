849b171583694ad4001d74e790bce9dbff005e12
Daniel Snider <danielsnider12@gmail.com>
2013-06-30 07:21:04 +0200

    created file which acts as webserver and includes webframework code

---
+++ server.js
@@ -0,0 +1,46 @@
+
+/**
+ * web framework code
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , upload = require('./routes/upload')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 80);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser({keepExtensions: true, uploadDir: __dirname + "/public/uploads"}));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
+/**
+ * webserver code
+ */
+
+var connect = require('connect');
+
+connect.createServer(
+    connect.static(__dirname)
+).listen(8080);
+
