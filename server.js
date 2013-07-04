82191aa3f4a4f0d3c810ae4db55b161f37f8e539
Daniel Snider <danielsnider12@gmail.com>
2013-07-04 11:03:17 +0200

    change test

--- server.js
+++ server.js
@@ -131,7 +131,6 @@ app.get('/restart', function(req, res) {
 
 http.createServer(app).listen(app.get('port'), function(){
   // connect.static(__dirname)
-	//test!!
   console.log("Express server listening on port " + app.get('port'));
 });
 
